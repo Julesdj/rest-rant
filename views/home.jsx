8cfaf63ac2b7ebb1d1d0ff1d41c682ddfb99a90a
Kokutse DJOGUENOU <kokutse@Kokutses-MacBook-Pro.local>
2022-03-28 17:41:10 -0400

    Adding bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,15 @@ function home () {
         <Def>
             <main>
                 <h1>Home</h1>
+                <div>
+                    <img src="/images/A-plate-of-salade.jpg" alt="A bowl of salade" />
+                    <div>
+                    Photo by <a href="https://unsplash.com/@annapelzer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anna Pelzer</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
+                <a href="/places">
+                <button className="btn-primary">Places Page</button>
+                </a>
             </main>
         </Def>
     )
