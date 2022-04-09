0f1e80b9f22276cb7b4844cdfd65a5c1bed269f4
Kokutse DJOGUENOU <kokutse@Kokutses-MacBook-Pro.local>
2022-04-09 11:51:35 -0400

    Adding input validations

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -30,7 +30,7 @@ function new_form () {
             </div>
             <div className="form-group">
                 <label htmlfor="founded">Founded Year</label>
-                <input className="form-control" id="founded" name="founded" />
+                <input className="form-control" type="number" id="founded" name="founded" value={new Date().getFullYear()}/>
             </div>
             <input className="btn btn-primary" type="submit" value="Add A Place" />
             </form>
